a85984237ad1c9c97b5ce4e4f9a7826708a34d8b
as-obs <anatoli.schwarz@odoo-bs.com>
2023-01-12 16:31:51 +0100

    [IMP] added manifest strings

--- obs_pulsarav_project/manifest.py
+++ obs_pulsarav_project/manifest.py
@@ -3,6 +3,8 @@
     'version': '1.0',
     'author': 'OBS',
     'category': 'Project',
+    'application": True,
+    'installable": True,
     'depends': ['base','project'],
     'data': [
         'views/project_task_view.xml',
