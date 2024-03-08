abb32992c41771eb5ef06b3042180c3c901cf4dc
Patrick Heuel <patrick.heuel@obs-solutions.com>
2024-03-08 09:21:53 +0100

    [REM] remove DATEV modules for the time of the upgrade

--- Syscoon/syscoon_financeinterface_enterprise/README.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 16.0.0.0.1
-
-- Code migration to v16
--- Syscoon/syscoon_partner_customer_supplier_number/__init__.py
+++
@@ -1,3 +0,0 @@
-# © 2023 syscoon GmbH (<https://syscoon.com>)
-# License OPL-1, See LICENSE file for full copyright and licensing details.
-from . import models
